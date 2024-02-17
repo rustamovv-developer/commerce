269c8621fc80213640db092264d471aa093de14f
--global <--global>
2024-02-17 15:28:42 +0300

    adding password

--- js/pages.js
+++ js/pages.js
@@ -388,3 +388,14 @@ function logout(event) {
 
   location.reload();
 }
+
+// password
+const togglePassword = document.querySelector("#toggle-password");
+const password = document.querySelector("#password");
+
+togglePassword.addEventListener("click", function () {
+  const type =
+    password.getAttribute("type") === "password" ? "text" : "password";
+  password.setAttribute("type", type);
+  this.classList.toggle("bi-eye");
+});
